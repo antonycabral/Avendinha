a4e62931118612773ba65793cd68ac955d4f421a
Antony Guilherme <aglc887@gmail.com>
2024-07-04 17:24:38 -0300

    feat crud

--- src/main/java/com/avendinha/repository/CustomerRepository.java
+++ src/main/java/com/avendinha/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.avendinha.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
---
+++ src/main/java/com/avendinha/service/orderService.java
@@ -0,0 +1,5 @@
+package com.avendinha.service;
+
+public class orderService {
+    // gerar crud
+}
